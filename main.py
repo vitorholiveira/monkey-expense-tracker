952ad311fad637746c4fb014f25bf16ee4d9bd00
Vítor Hugo Magnus Oliveira <vitorh.magnus.oliveira@gmail.com>
2025-09-13 14:09:53 -0300

    feat: modularize update_expanse and add choices to category arg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from datetime import datetime
+from pathlib import Path
 
 import pandas as pd
 from dotenv import load_dotenv
@@ -23,6 +24,33 @@ def calculate_amount_left(expense_df: pd.DataFrame):
     print(f"\n==> You have {CURRENCY} {amount_left:.2f} left.\n")
 
 
+def update_expense(
+    expense_filepath: Path,
+    name: str,
+    category: str,
+    amount: float,
+    description: str,
+    date: str,
+) -> pd.DataFrame:
+    args_list = [name, category, amount, description, date]
+    if os.path.exists(expense_filepath):
+        expense_df = pd.read_csv(expense_filepath)
+        new_expense_row = pd.DataFrame(
+            [args_list],
+            columns=expense_df.columns,
+        )
+        expense_df = pd.concat([expense_df, new_expense_row], ignore_index=True)
+    else:
+        expense_df = pd.DataFrame(
+            [args_list],
+            columns=EXPENSE_DF_COLUMNS,
+        )
+    expense_df.to_csv(expense_filepath, index=False)
+    print(expense_df)
+    print(f"\nExpense added successfully to {expense_filepath}")
+    return expense_df
+
+
 def main():
     date = datetime.now()
     month = str(date.month) if date.month >= 10 else f"0{date.month}"
@@ -42,69 +70,43 @@ def main():
         "-n",
         "--name",
         required=True,
+        type=str,
         help="The name of the expense. Need to be a string.",
     )
     expense_parser.add_argument(
         "-c",
         "--category",
         required=True,
-        help=f"The category of the expense that should be one of {EXPENSE_CATEGORIES}.",
+        choices=EXPENSE_CATEGORIES,
+        type=str,
+        help="The category of the expense.",
     )
     expense_parser.add_argument(
         "-a",
         "--amount",
         required=True,
         type=float,
-        help="The monetary amount of the expense. Need to be an integer or a float.",
+        help="The monetary amount of the expense. Need to be an integer or float.",
     )
     expense_parser.add_argument(
         "-d",
         "--description",
         default="NO DESC",
+        type=str,
         help="An optional description for the expense. Need to be a string.",
     )
 
     # Parse the arguments
     args = expense_parser.parse_args()
 
-    # Validate category
-    if args.category not in EXPENSE_CATEGORIES:
-        print(f"Type argument should be one of {EXPENSE_CATEGORIES}")
-        return
-
-    # Create or update expense DataFrame
-    if os.path.exists(expense_filepath):
-        expense_df = pd.read_csv(expense_filepath)
-        new_expense_row = pd.DataFrame(
-            [
-                [
-                    args.name,
-                    args.category,
-                    args.amount,
-                    args.description,
-                    date.strftime("%Y-%m-%d"),
-                ]
-            ],
-            columns=expense_df.columns,
-        )
-        expense_df = pd.concat([expense_df, new_expense_row], ignore_index=True)
-    else:
-        expense_df = pd.DataFrame(
-            [
-                [
-                    args.name,
-                    args.category,
-                    args.amount,
-                    args.description,
-                    date.strftime("%Y-%m-%d"),
-                ]
-            ],
-            columns=EXPENSE_DF_COLUMNS,
-        )
-
-    expense_df.to_csv(expense_filepath, index=False)
-    print(expense_df)
-    print(f"\nExpense added successfully to {expense_filepath}")
+    expense_df = update_expense(
+        expense_filepath,
+        args.name,
+        args.category,
+        args.amount,
+        args.description,
+        date.strftime("%Y-%m-%d"),
+    )
     calculate_amount_left(expense_df)
 
 
