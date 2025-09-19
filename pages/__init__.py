8273daa564a8d7c6d6c4d79b6b6fbec574ed69b1
Vítor Hugo Magnus Oliveira <vitorh.magnus.oliveira@gmail.com>
2025-09-19 09:53:45 -0300

    chore: remove useless __init__.py file

--- pages/__init__.py
+++
@@ -1,4 +0,0 @@
-from .custom_range_spending import custom_range_spending_page
-from .monthly_spending import monthly_spending_page
-
-__all__ = ["monthly_spending_page", "custom_range_spending_page"]
